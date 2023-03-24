bf80fa35ac1317b2620d3f0666d1aaae97e4ba92
Ridwan Monjur <mjrrdn@gmail.com>
2023-03-25 07:51:48 +0800

    feat(footer_: center align

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,17 +1,7 @@
-const links = [
-  { href: '/', label: 'Home' },
-  { href: '#about', label: 'About' },
-  { href: '#project', label: 'Projects' },
-  { href: '#contact', label: 'Contacts' },
-];
-const myName = 'ridwan.dev';
-
 export default function Footer() {
   return (
-    <footer className="footer items-center text-center lg:text-left py-4 px-16 bg-slate-50  text-gray-500">
-      <div className="items-center text-center grid-flow-col">
-        <p>Copyright © Ridwan, {new Date().getFullYear()} - All right reserved</p>
-      </div>
+    <footer className="items-center text-center py-4 px-16 bg-slate-50  text-gray-500">
+        <p>Copyright © Ridwan  Bin Monjur, {new Date().getFullYear()} - All right reserved</p>
     </footer>
   );
 }
